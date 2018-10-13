f0cea6b26402c7ebcc9699ba888059de143d568b
Yev Barkalov <yevbar1729@gmail.com>
2018-10-12 23:36:53 -0700

    Add build directory

---
+++ app/react_app/build/precache-manifest.46874d9fb44e920f7d9986d97081b83c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "50c450609dd029ffcb6d",
+    "url": "/static/js/main.50c45060.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "50c450609dd029ffcb6d",
+    "url": "/static/css/main.50c99fb5.chunk.css"
+  },
+  {
+    "revision": "da90dc1b9f9323a81e883bb0ae9ad3e3",
+    "url": "/index.html"
+  }
+];

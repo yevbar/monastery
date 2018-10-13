b1b5c7d0dd36badc2c9938b51bb4b4966bd699b8
Yev Barkalov <yevbar1729@gmail.com>
2018-10-12 22:10:30 -0700

    Add boilerplate for Flask-PyMongo

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,10 @@
 from flask import Flask
+from flask_pymongo import PyMongo
+import os
 
 app = Flask(__name__)
+app.config["MONGO_URI"] = os.environ["MONGODB_URI"]
+mongo = PyMongo(app)
 
 @app.route("/")
 def index():

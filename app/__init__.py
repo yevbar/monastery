248851ece8ab1ff41fd4011e61e1ac0f2f1ff813
Yev Barkalov <yevbar1729@gmail.com>
2018-10-12 23:31:41 -0700

    Serve React.js file

--- app/__init__.py
+++ app/__init__.py
@@ -1,19 +1,21 @@
 from bson.json_util import dumps
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, send_from_directory
 from flask_pymongo import PyMongo
 from json import loads
 import os
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='./react_app/build')
 app.config["MONGO_URI"] = os.environ["MONGODB_URI"]
 mongo = PyMongo(app)
 
-"""
-Root url, will be home page
-"""
-@app.route("/")
-def index():
-    return "Hello from Flask!"
+# Serve React App
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def serve(path):
+    if path != "" and os.path.exists("./react_app/build/" + path):
+        return send_from_directory('./react_app/build', path)
+    else:
+        return send_from_directory('./react_app/build', 'index.html')
 
 """
 Database test, pass in /database?val=VALUE

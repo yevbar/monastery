df654c8c5c1a1f25a63cecc60b18b12ea6df5617
Yev Barkalov <yevbar1729@gmail.com>
2018-10-12 22:30:18 -0700

    Add database test

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 from flask_pymongo import PyMongo
 import os
 
@@ -6,9 +6,30 @@ app = Flask(__name__)
 app.config["MONGO_URI"] = os.environ["MONGODB_URI"]
 mongo = PyMongo(app)
 
+"""
+Root url, will be home page
+"""
 @app.route("/")
 def index():
     return "Hello from Flask!"
+
+"""
+Database test, pass in /database?val=VALUE
+
+Functionality: Takes in val argument, appends to collection, and returns entire collection
+"""
+@app.route("/database")
+def database_test():
+   args = request.args
+   val = args["val"]
+   mongo.db.vals.insert_one({
+       "val": val
+    })
+   vals = mongo.db["vals"]
+   cursor = vals.find({})
+   for document in cursor:
+       print(document)
+   return jsonify({"message": "success"}) 
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
